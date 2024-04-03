51554049d92131dbb97976413ea63954bec67c9b
mancy <m@m.m.com>
2024-04-03 06:28:00 +0300

    n

--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('profile/', views.profile,name="profile"),
     path('logout/', views.logout, name="logout"),
-    path('', views.main, name="main"),
-    path('athkar/<int:num>', views.athkar, name="athkar"),
+    path('/', views.main, name="main"),
+    path('athkar/<int:num>/', views.athkar, name="athkar"),
     path('add/', views.thkr, name="add"),
     path('athkar/', views.main, name="main"),
-    path('/<int:num>', views.athkar, name="athkar"),
+    path('/<int:num>/', views.athkar, name="athkar"),
 ]
